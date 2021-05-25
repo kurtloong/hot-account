69f2be0a5fa2eba56c5f03122795acfdf1b93706
kurt.loong <kurt.loong@foxmail.com>
2021-05-25 20:02:39 +0800

    Initial commit

---
+++ src/test/java/com/github/kurtloong/hotaccount/HotAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kurtloong.hotaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
